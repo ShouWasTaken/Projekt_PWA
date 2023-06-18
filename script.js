14c24636f53a98b4ee29e2cfe81207fe7abb039f
ShouWasTaken <mwitkowski1611@gmail.com>
2023-06-18 15:56:52 +0200

    Polishing

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const registrationButton = document.getElementById('registration-button');
+const welcomeMessage = document.getElementById('welcome-message');
+const logoutButton = document.getElementById('logout-button');
 const modalRegistration = document.getElementById('registerModal');
 const closeRegistration = document.querySelector('.modal-content .close');
 
 registrationButton.addEventListener('click', openRegistrationModal);
 closeRegistration.addEventListener('click', closeRegistrationModal);
-window.addEventListener('click', outsideClickRegistration);
 
 function openRegistrationModal() {
   modalRegistration.style.display = 'block';
@@ -14,20 +15,34 @@ function closeRegistrationModal() {
   modalRegistration.style.display = 'none';
 }
 
-function outsideClickRegistration(e) {
-  if (e.target === modalRegistration) {
-    modalRegistration.style.display = 'none';
-  }
+function register(event) {
+  event.preventDefault();
+
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+
+  const name = nameInput.value;
+  welcomeMessage.textContent = 'Witaj, ' + name;
+  welcomeMessage.classList.remove('hidden');
+
+  registrationButton.classList.add('hidden');
+  logoutButton.classList.remove('hidden');
+
+  nameInput.value = '';
+  emailInput.value = '';
+  passwordInput.value = '';
+
+  closeRegistrationModal();
 }
 
 
 const weatherButton = document.getElementById('weather-button');
 const modalWeather = document.getElementById('weatherModal');
-const closeWeather = document.querySelectorAll('.weather-check .close')[0];
+const closeWeather = document.querySelectorAll('.weather-check .close')[1];
 
 weatherButton.addEventListener('click', openWeatherModal);
 closeWeather.addEventListener('click', closeWeatherModal);
-window.addEventListener('click', outsideClickWeather);
 
 function openWeatherModal() {
   modalWeather.style.display = 'block';
@@ -42,6 +57,31 @@ function outsideClickWeather(e) {
     modalWeather.style.display = 'none';
   }
 }
+
+
+registrationButton.addEventListener('click', openRegistrationModal);
+logoutButton.addEventListener('click', logout);
+
+function logout() {
+  welcomeMessage.classList.add('hidden');
+  registrationButton.classList.remove('hidden');
+  logoutButton.classList.add('hidden');
+}
+
+
+const closeModalButtons = document.querySelectorAll('#registerModal .close');
+
+for (var i = 0; i < closeModalButtons.length; i++) {
+  closeModalButtons[i].addEventListener('click', closeRegistrationModal);
+}
+
+
+const registerForm = document.getElementById('registerForm');
+const weatherForm = document.getElementById('weatherForm');
+
+registerForm.addEventListener('submit', register);
+weatherForm.addEventListener('submit', getWeather);
+
 
 const input = document.querySelector('input');
 const button = document.querySelector('button');
@@ -62,41 +102,50 @@ const apiKey = '&appid=849361ccd38ac268f0c96aa68c6686ce';
 const apiUnits = '&units=metric';
 const apiLang = '&lang=pl';
 
-function getWeather(){
+function getWeather() {
   const apiCity = input.value;
   const URL = apiLink + apiCity + apiKey + apiUnits + apiLang;
 
   axios.get(URL).then(response => {
-      console.log(response.data);
-      cityName.textContent = `${response.data.name}, ${response.data.sys.country}`;
-      img.src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`;
-      temperature.textContent = `${Math.round(response.data.main.temp)} C`;
-      temperatureDescription.textContent = `${response.data.weather[0].description}`;
-      feelsLike.textContent = `${Math.round(response.data.main.feels_like)}`;
-      pressure.textContent = `${response.data.main.pressure} hPa`;
-      humidity.textContent = `${response.data.main.humidity} %`;
-      windSpeed.textContent = `${Math.round((response.data.wind.speed) * 3,6)} km/h`;
-      clouds.textContent = `${response.data.clouds.all} %`;
-      errorMessage.textContent = '';
+    console.log(response.data);
+    cityName.textContent = `${response.data.name}, ${response.data.sys.country}`;
+    img.src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`;
+    temperature.textContent = `${Math.round(response.data.main.temp)} C`;
+    temperatureDescription.textContent = `${response.data.weather[0].description}`;
+    feelsLike.textContent = `${Math.round(response.data.main.feels_like)}`;
+    pressure.textContent = `${response.data.main.pressure} hPa`;
+    humidity.textContent = `${response.data.main.humidity} %`;
+    windSpeed.textContent = `${Math.round(response.data.wind.speed * 3.6)} km/h`;
+    clouds.textContent = `${response.data.clouds.all} %`;
+    errorMessage.textContent = '';
   }).catch(error => {
-      console.log("error");
-      if(error.response.data.cod !== '200'){
-          errorMessage.textContent = `${error.response.data.message}`;
-      }
-      [clouds, windSpeed, humidity, pressure, cityName, temperature, temperatureDescription, feelsLike].forEach(el => {
-          el.textContent = '';
-      })
-      img.src = '';
+    console.log("error");
+    if (error.response.data.cod !== '200') {
+      errorMessage.textContent = `${error.response.data.message}`;
+    }
+    [clouds, windSpeed, humidity, pressure, cityName, temperature, temperatureDescription, feelsLike].forEach(el => {
+      el.textContent = '';
+    })
+    img.src = '';
   }).finally(() => {
-      input.value = '';
+    input.value = '';
   })
 }
 
 function getWeatherByEnter(e) {
-  if(e.key === 'Enter'){
-      getWeather();
+  if (e.key === 'Enter') {
+    getWeather();
   }
 }
 
 input.addEventListener('keypress', getWeatherByEnter)
 button.addEventListener('click', getWeather);
+
+
+const closeWeatherButtons = document.querySelectorAll('#weatherModal .close');
+
+for (var i = 0; i < closeWeatherButtons.length; i++) {
+  closeWeatherButtons[i].addEventListener('click', closeWeatherModal);
+}
+
+window.addEventListener('click', outsideClickWeather);
